2297aaa59a13498c8fad97ca06c8f9dde60eabff
Priti Bedre <pritinbedre22@gmail.com>
2026-01-26 00:32:33 +0530

    Use environment variables for DB connection

--- src/main/java/com/ConnectionFactory/ConnectionFactory.java
+++ src/main/java/com/ConnectionFactory/ConnectionFactory.java
@@ -6,18 +6,14 @@ import java.sql.SQLException;
 
 public class ConnectionFactory {
 
-    // PostgreSQL connection details
-    private static final String URL = "jdbc:postgresql://dpg-d5lr05je5dus73e1c020-a.singapore-postgres.render.com:5432/todoapp_db_e1fa";
-    private static final String USER = "todoapp_db_e1fa_user";
-    private static final String PASSWORD = "YcHLWCOIHVztudWSWamH7ORfMmXxCHZE";
+    private static final String URL = System.getenv("DB_URL");
+    private static final String USER = System.getenv("DB_USER");
+    private static final String PASSWORD = System.getenv("DB_PASSWORD");
 
     public static Connection getConnection() {
         Connection con = null;
         try {
-            // Load PostgreSQL JDBC Driver
             Class.forName("org.postgresql.Driver");
-
-            // Connect to database
             con = DriverManager.getConnection(URL, USER, PASSWORD);
             System.out.println("PostgreSQL Connected Successfully ✅");
         } catch (ClassNotFoundException e) {
